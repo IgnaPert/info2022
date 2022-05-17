83aeb7f74302308de68159b928714c869663b96f
ignapert@gmail.com <ignapert@gmail.com>
2022-05-17 10:53:39 -0300

    Carpeta componente y noticia agregado

---
+++ src/components/Noticia.js
@@ -0,0 +1,16 @@
+
+
+function Noticia() {
+  return (
+    <div className="Noticia">
+      <header className="App-header"><title>Título</title>
+      </header>
+
+      <body>
+        Descripción
+      </body>
+    </div>
+  );
+}
+
+export default Noticia;
